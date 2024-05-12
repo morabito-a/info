615ae2401b36eed8e70607f1406115cd15342e17
Alessandro <92431992+morabito-a@users.noreply.github.com>
2024-05-12 23:43:36 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,52 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Brussels, Belgium",
+    50.812430,
+    4.383357
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Theran, Iran",
+    35.725610, 
+    51.385647
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Vienna, Austria",
+    48.208183,
+    16.363261
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Tucurui, Brazil",
+    -3.830638, 
+    -49.663589
+  ]
+  [
+    "Brazilia, Brazil",
+    -15.764091, 
+    -47.868346
+  ]
+  [
+    "Panama City, Panamá",
+    8.983001,
+    -79.527915
+  ]
+  [
+    "Rio de Janeiro, Brazil",
+    -22.966451, 
+    -43.183199
+  ]
+  [
+    "Västerås, Sweden",
+    59.618177, 
+    16.539828
+  ]
+  [
+  "Porto, Portugal",
+  41.156648,
+  -8.643291
+  ]
+  [
+  "Liege, Belgium",
+  50.641854, 
+  5.571714
   ]
 ];
