355b0395a583ab6eda7d5b830455b2fe8e1d2b37
Alessandro <92431992+morabito-a@users.noreply.github.com>
2024-05-12 23:51:25 +0200

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -18,32 +18,32 @@ var addressPoints = [
     "Tucurui, Brazil",
     -3.830638, 
     -49.663589
-  ]
+  ],
   [
     "Brazilia, Brazil",
     -15.764091, 
     -47.868346
-  ]
+  ],
   [
     "Panama City, Panamá",
     8.983001,
     -79.527915
-  ]
+  ],
   [
     "Rio de Janeiro, Brazil",
     -22.966451, 
     -43.183199
-  ]
+  ],
   [
     "Västerås, Sweden",
     59.618177, 
     16.539828
-  ]
+  ],
   [
   "Porto, Portugal",
   41.156648,
   -8.643291
-  ]
+  ],
   [
   "Liege, Belgium",
   50.641854, 
